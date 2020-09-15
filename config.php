06c5ece7b901081568509bb3eaca895f453501b1
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-16 00:18:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
         'api' => 'http://142.93.201.191:13281',
-        'blockTargetInterval' => 120,
+        'blockTargetInterval' => 90,
         'coinUnits' => 85000000000000
 );
 
