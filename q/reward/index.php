35a3dd1a774db637ce8eba365cde08d4d4e0aac6
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-16 00:26:44 +0200

    Update index.php

--- q/reward/index.php
+++ q/reward/index.php
@@ -12,7 +12,7 @@ $config = (require '../../config.php');
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,  $config['api'].'/getinfo');
+curl_setopt($ch, CURLOPT_URL,  $config['api'].'/info');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 curl_close($ch);
