793a3b21d021bf8fa9ee25f0a2e4c00e29634c00
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-16 22:47:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,22 +8,22 @@ var refreshDelay = 30000;
 //NB do not include / at the end of url
 var networkStat = {
  "cpa": [
-["pool.cryptopay.org.za", "https://pool.cryptopay.org.za:8119"],
-["cpa.thepiratemine.nl", "https://cpa.piratepools.nl:2887"],
-["miningcache.com", "https://miningcache.com:8119"],
-["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119"],
-["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119"],
-["cpa.cnhub.de", "http://cpa.cnhub.de:8127"],
-["cpa.multiminer.us", "https://cpa.multiminer.us:8119"],
-["cpa.cryptomine.cx", "https://cpa.cryptomine.cx:8001"],
-["cpa.optimusblue.com","https://cpa.optimusblue.com:8129"],
-["cpa.youpool.io","http://cpa.youpool.io:8135"]
+["poolparty.cryptopay.org.za", "https://partystats.cryptopay.org.za"],
+//["cpa.thepiratemine.nl", "https://cpa.piratepools.nl:2887"],
+//["miningcache.com", "https://miningcache.com:8119"],
+//["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119"],
+//["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119"],
+//["cpa.cnhub.de", "http://cpa.cnhub.de:8127"],
+//["cpa.multiminer.us", "https://cpa.multiminer.us:8119"],
+//["cpa.cryptomine.cx", "https://cpa.cryptomine.cx:8001"],
+//["cpa.optimusblue.com","https://cpa.optimusblue.com:8129"],
+//["cpa.youpool.io","http://cpa.youpool.io:8135"]
  ]
 };
 
 var networkStat2 = {
   "cpa": [
-    ["cpa.romining.farm", "https://cpa.romining.farm/api"],
+  //  ["cpa.romining.farm", "https://cpa.romining.farm/api"],
 
   ]
 };
