dcbd7c74346ab1de9ae8d75aa8a39db89b8974fc
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-11-19 11:25:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var networkStat = {
 ["poolparty.cryptopay.org.za", "https://partystats.cryptopay.org.za"],
 ["letshash.it","https://letshash.it:8273"],
 ["cpacoin.my-mining-pool.de", "https://apicpacoin.my-mining-pool.de"]
-//["miningcache.com", "https://miningcache.com:8119"],
+["newpool.pw/cpa", "https://minenice.newpool.pw:8215/stats"],
 //["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119"],
 //["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119"],
 //["cpa.cnhub.de", "http://cpa.cnhub.de:8127"],
